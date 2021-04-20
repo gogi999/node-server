1fc766c467de539944df71684e7e800a14616832
gogi999 <g.petrovic9@gmail>
2021-04-20 12:26:35 +0200

    Updated src/index.js

--- src/index.js
+++ src/index.js
@@ -8,15 +8,6 @@ const PORT = 5000;
 const app = express();
 router(app);
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(bodyParser.json())
-
-// Helmet helps you secure your Express apps by setting various HTTP headers
-app.use(helmet());
-
 app.listen(PORT, (error) => {
     if (error) return console.log(error);
     console.log(`Server is listening on port ${PORT}...`);
